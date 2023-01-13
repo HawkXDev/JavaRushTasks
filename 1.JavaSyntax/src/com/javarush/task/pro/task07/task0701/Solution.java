d815b57015232216500f228d9fa3cdbc489dacc1
sokolserg <sergsokolkin@gmail.com>
2023-01-13 16:59:19 +0300

    do java_syntax 03

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int i;
+    double d;
+    boolean b;
+    char c;
+    float f;
+    byte bt;
+    long l;
+    short sr;
+}
