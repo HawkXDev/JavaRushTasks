70c5d561da4c79101679773ee1f54eba2876df82
sokolserg <sergsokolkin@gmail.com>
2023-01-17 11:59:04 +0300

    do java_syntax 18

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task09.task0914;
+
+/* 
+Обновление пути
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin/";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        int start = path.indexOf("jdk");
+        int end = path.indexOf("/", start + 1);
+        return path.substring(0, start) + jdk + path.substring(end);
+    }
+}
