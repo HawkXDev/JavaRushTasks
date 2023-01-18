6ac3316551044dc6782b6f883817031328ddbe95
sokolserg <sergsokolkin@gmail.com>
2023-01-18 19:22:21 +0300

    do java_syntax 20

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1012/Memory.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task10.task1012;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/* 
+Дефрагментация памяти
+*/
+
+public class Memory {
+
+    public static void main(String[] args) {
+        String[] memory = {"object15", null, null, "object2", null, null, null, "object32", null, "object4"};
+        executeDefragmentation(memory);
+        System.out.println(Arrays.toString(memory));
+    }
+
+    public static void executeDefragmentation(String[] array) {
+        for (int j = 0; j < array.length - 1; j++) {
+            boolean areThereNotNull = true;
+            while (array[j] == null && areThereNotNull) {
+                for (int k = j + 1; k < array.length; k++) {
+                    array[k - 1] = array[k];
+                }
+                array[array.length - 1] = null;
+                areThereNotNull = false;
+                for (int i = j + 1; i < array.length - 1; i++) {
+                    if (array[i] != null) {
+                        areThereNotNull = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
