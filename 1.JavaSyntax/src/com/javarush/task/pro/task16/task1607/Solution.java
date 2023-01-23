3ff73f2923babbef9638bb625c9768a433d2d5fb
sokolserg <sergsokolkin@gmail.com>
2023-01-23 21:00:17 +0300

    do java_syntax 27

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1607/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task16.task1607;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/* 
+Освоение нового API
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(nowExample());
+        System.out.println(ofExample());
+        System.out.println(ofYearDayExample());
+        System.out.println(ofEpochDayExample());
+    }
+
+    static LocalDate nowExample() {
+        return LocalDate.now();
+    }
+
+    static LocalDate ofExample() {
+        return LocalDate.of(2020, Month.SEPTEMBER, 12);
+    }
+
+    static LocalDate ofYearDayExample() {
+        LocalDate date = LocalDate.of(2020, Month.SEPTEMBER, 12);
+        return LocalDate.ofYearDay(2020, date.getDayOfYear());
+    }
+
+    static LocalDate ofEpochDayExample() {
+        LocalDate date = LocalDate.of(2020, Month.SEPTEMBER, 12);
+        return LocalDate.ofEpochDay(date.toEpochDay());
+    }
+}
