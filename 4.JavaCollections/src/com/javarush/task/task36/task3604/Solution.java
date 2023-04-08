742ac8a7ac4a3f978cc5979ca4657b0a29cdcee8
sokolserg <sergsokolkin@gmail.com>
2023-04-08 21:43:33 +0300

    RedBlackTree

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3604/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task36.task3604;
+
+/* 
+Разбираемся в красно-черном дереве
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        RedBlackTree redBlackTree = new RedBlackTree();
+        redBlackTree.insert(5);
+        redBlackTree.insert(10);
+        redBlackTree.insert(2);
+        redBlackTree.insert(10);
+    }
+}
